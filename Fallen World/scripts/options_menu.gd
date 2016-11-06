6c5bc5adb94c4667134677175765765b5a522e34
Unknown <casper911.rim@gmail.com>
2016-11-06 20:03:51 +0100

    Clear

--- Fallen World/scripts/options_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_fullscreen_toggled( pressed ):
-	OS.set_window_fullscreen( pressed )
-	get_node("fullscreen").set_disabled( !pressed )
-
-func _on_back_pressed():
-	get_tree().change_scene("res://main_menu.tscn")
